d6965392ec24a9ead2b4021eb8ad5f66281bb19d
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-05-07 14:34:27 +0900

    Feat : swagger 서버 추가

--- src/main/java/goormthon/hufs/chulcheck/config/SwaggerConfig.java
+++ src/main/java/goormthon/hufs/chulcheck/config/SwaggerConfig.java
@@ -1,14 +1,10 @@
 package goormthon.hufs.chulcheck.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
@@ -20,15 +16,10 @@ public class SwaggerConfig {
 			.version("1.0")
 			.description("API for managing users and their profile images.");
 
-		String localServer = "http://localhost:8080";
-
 		return new OpenAPI()
-			.info(info)
-			.servers(List.of(
-				new Server().url(localServer).description("Local Server")
-			))
-			.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-			.components(new io.swagger.v3.oas.models.Components());
+			.info(info);
+		// .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+		// .components(new io.swagger.v3.oas.models.Components());
 		// .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
 		// 	.type(SecurityScheme.Type.HTTP)
 		// 	.scheme("bearer")
