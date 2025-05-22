bae93b00dd3d6c2c7ae102069f5966d588803b0d
leestana01 <leestana01@naver.com>
2025-05-22 18:06:01 +0900

    refactor: 오타 수정 및 전역 RequestMapping 설정

--- src/main/java/goormthon/hufs/chulcheck/controller/AttendanceController.java
+++ src/main/java/goormthon/hufs/chulcheck/controller/AttendanceController.java
@@ -6,38 +6,39 @@ import goormthon.hufs.chulcheck.domain.dto.response.GetAttendanceResponse;
 import goormthon.hufs.chulcheck.domain.dto.response.GetAttendanceStatsResponse;
 import goormthon.hufs.chulcheck.service.AttendanceService;
 import java.util.List;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/v1/attendance")
 @RestController
 @RequiredArgsConstructor
-public class AttendenceController {
+public class AttendanceController {
     private final AttendanceService attendanceService;
 
-    @PostMapping("/attendence")
-    public void attendence(Authentication authentication,
+    @PostMapping
+    public void attendance(Authentication authentication,
                            @RequestBody CreateAttendenceRequest request) {
         CustomOAuth2User customOAuth2User = (CustomOAuth2User)authentication.getPrincipal();
         String userId = customOAuth2User.getUserId();
         attendanceService.createAttendance(userId, request.getSessionId(), request.getCode());
     }
 
-    @GetMapping("/attendence")
-    public List<GetAttendanceResponse> getAllAttendences(Authentication authentication,
+    @GetMapping
+    public List<GetAttendanceResponse> getAllAttendances(Authentication authentication,
                                                      @RequestParam Long clubId) {
         CustomOAuth2User customOAuth2User = (CustomOAuth2User)authentication.getPrincipal();
         String userId = customOAuth2User.getUserId();
         return attendanceService.getAllAttendancesByUserAndClub(userId, clubId);
     }
 
-    @GetMapping("/attendence/stats")
-    public GetAttendanceStatsResponse getAttendenceStats(Authentication authentication,
+    @GetMapping("/stats")
+    public GetAttendanceStatsResponse getAttendanceStats(Authentication authentication,
                                                          @RequestParam Long clubId) {
         CustomOAuth2User customOAuth2User = (CustomOAuth2User)authentication.getPrincipal();
         String userId = customOAuth2User.getUserId();
